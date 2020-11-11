06f35c5c41941618f3dcc2d538e1906d1bceb872
Kenichi Sakamoto <eug.ken0601@gmail.com>
2020-11-11 00:51:26 -0800

    bug fixed.

--- generator.py
+++ generator.py
@@ -8,9 +8,18 @@ result = [] #holds all the json data
 def add_to_json(list):
   with open('sample.json') as jsonfile:
     data = json.load(jsonfile)
+    rem = [] #temporarily stores attributes that we need to remove 
     for eachData in list:
       temp = copy.deepcopy(data) #deep copy the original 
-      temp.update(eachData) #update values 
+      for i in eachData:
+        if i in temp['stations']:
+          temp['stations'][i] = eachData[i]
+          rem.append(i) 
+        temp.update(eachData) #update values 
+         
+        for i in rem: #iterate thru rem and find duplicates in temp. remove that
+          if i in temp:
+            del temp[i]
       result.append(temp) 
   
   with open("freeway.json","w") as outfile:
