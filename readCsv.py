a0528a6ddcf65b6b706949350878ce449afb002d
Kenichi Sakamoto <eug.ken0601@gmail.com>
2020-11-10 23:18:29 -0800

    read csv and write to json

--- readCsv.py
+++ readCsv.py
@@ -1,5 +1,29 @@
 import csv
-with open("freeway_stations.csv", newline='') as csvfile:
-  reader = csv.DictReader(csvfile)
-  for row in reader:
-    print(row['stationid'],row["highwayid"],row["milepost"],row["locationtext"],row["upstream"],row["downstream"],row["stationclass"],row["numberlanes"],row["latlon"],row["length"])
+import json
+import copy
+
+list = [] #store data from the file
+result = [] #holds all the json data 
+
+def add_to_json(list):
+  with open('sample.json') as jsonfile:
+    data = json.load(jsonfile)
+    for eachData in list:
+      temp = copy.deepcopy(data) #deep copy the original 
+      temp.update(eachData) #update values 
+      result.append(temp) 
+  
+  for itr in result:
+    print(itr)
+
+
+def read_csv():
+  with open("freeway_stations.csv", newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      list.append(row)
+  add_to_json(list)
+
+read_csv()
+
+
