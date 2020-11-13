557ce8ee5be64d4fc5622cc8ca131769ae1d96c2
Kenichi Sakamoto <eug.ken0601@gmail.com>
2020-11-13 14:00:35 -0800

    created folders

--- generator.py
+++ generator.py
@@ -9,7 +9,7 @@ result = [] #holds all the json data
 
 #add data into new json file called "freeway.json"
 def generator(list,highwayList,detectorList):
-  with open('sample.json') as jsonfile:
+  with open('json/sample.json') as jsonfile:
     data = json.load(jsonfile)
     rem = [] #temporarily stores attributes that we need to remove 
     temp = None
@@ -40,7 +40,7 @@ def generator(list,highwayList,detectorList):
 
   
   #iterate through result and put all the json data into freeway.json
-  with open("stations.json","w") as outfile:
+  with open("json/stations.json","w") as outfile:
     for itr in result:
       json_obj = json.dumps(itr,indent=4) #add indent=4 to make it more readable
       outfile.write(json_obj+'\n\n')
@@ -48,7 +48,7 @@ def generator(list,highwayList,detectorList):
 
 #read freeway_stations.csv 
 def read_csv():
-  with open("freeway_stations.csv", newline='') as csvfile:
+  with open("csv/freeway_stations.csv", newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
       list.append(row)
@@ -56,7 +56,7 @@ def read_csv():
 
 #read highway
 def read_highway():
-  with open("highways.csv", newline='') as highway:
+  with open("csv/highways.csv", newline='') as highway:
     reader = csv.DictReader(highway)
     for row in reader:
       highwayList.append(row)      
@@ -64,7 +64,7 @@ def read_highway():
 
 #read freeway_detectors
 def read_detector():
-  with open("freeway_detectors.csv", newline='') as detector:
+  with open("csv/freeway_detectors.csv", newline='') as detector:
     reader = csv.DictReader(detector)
     for row in reader:
       detectorList.append(row)      
